e491e375ce64d487680a95da979d70309a214b39
papoose974 <alex-97.4@hotmail.fr>
2025-02-11 21:04:56 +0100

    vercel1

--- app/router.js
+++ app/router.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const mainController = require('./controllers/mainController');
 
-router.get('/', mainController.homePage);
+router.get("/", (req, res, next) => {
+    console.log("La route / est bien atteinte !");
+    next();
+}, mainController.homePage);
 router.get('/article/:id', mainController.articlePage);
 router.get('/boutique', mainController.boutiquePage);
 router.get('/catalogue', mainController.cataloguePage);
--- index.js
+++ index.js
@@ -27,5 +27,5 @@ db.query('SELECT NOW()', (err, res) => {
 app.use(router);
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
